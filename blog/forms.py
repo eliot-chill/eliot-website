336d536957f6a22b99c5a631ad18c5fd2e8cca90
Eliot Cholerton-Hill <eliotchill99@gmail.com>
2020-08-30 23:04:35 +0100

    Finished blog

---
+++ blog/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+from .models import Post
+from tinymce.widgets import TinyMCE
+
+class PostForm(forms.ModelForm):
+
+	text = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 30}))
+
+	class Meta:
+		model = Post
+
+		fields = ('title','text',)
